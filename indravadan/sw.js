399ac1421ea32c649d737c84f33b1da09e628eba
YVComputerclasses <yvcomputerclasses@gmail.com>
2026-08-15 01:30:21 +0530

    news1508

--- indravadan/sw.js
+++ indravadan/sw.js
@@ -1,4 +1,7 @@
-const CACHE_NAME = "ganesh-utsav-v2";
+// જૂનું: const CACHE_NAME = "ganesh-utsav-v1";
+// નવું:
+const CACHE_NAME = "ganesh-utsav-v2"; // વર્ઝન બદલવાથી નવો ડેટા ફેચ થશે
+
 const ASSETS_TO_CACHE = [
   "./",
   "./index.html",
@@ -9,8 +12,8 @@ const ASSETS_TO_CACHE = [
   "./manifest.json"
 ];
 
-// Service Worker Install
 self.addEventListener("install", (e) => {
+  self.skipWaiting(); // તરત જ નવો વર્ઝન એક્ટિવ કરવા
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS_TO_CACHE);
@@ -18,7 +21,21 @@ self.addEventListener("install", (e) => {
   );
 });
 
-// Fetching assets from cache
+self.addEventListener("activate", (e) => {
+  // જૂની કેશ ડિલીટ કરશે
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
