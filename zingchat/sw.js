7f1ff2a365463c6cfc4a56dcc5c3306ac7cd8c38
YVComputerclasses <yvcomputerclasses@gmail.com>
2026-08-26 17:12:34 +0530

    new26081712

---
+++ zingchat/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'zingchat-cache-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/image/Logo1.png'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch Event (Offline Cache Fallback)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
